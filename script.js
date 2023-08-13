b8afd0e54ffd11a7358b19e71e11289ab091bac2
Walter Hernandez <63087242+whernandezt@users.noreply.github.com>
2023-08-13 16:14:05 -0600

    Add compare function
    It's necesary to compare between the player selection and the computer
    choice and return the winner

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ const computerChoice = () => {
 
     return "SCISSORS";
 }
+
+const playRound = (player, computer) => {
+    if (computer === player)
+        return `It's a draw, both chose ${player}`;
+
+    let message = "";
+
+    switch(player){
+        case "ROCK":
+            message = computer === "SCISSORS" ? `You Win! ${player} beats ${computer}` : `You Lose! ${computer} beats ${player}`;
+            break;
+        case "PAPER":
+            message = computer === "ROCK" ? `You Win! ${player} beats ${computer}` : `You Lose! ${computer} beats ${player}`;
+            break;
+        case "SCISSORS":
+            message = computer === "PAPER" ? `You Win! ${player} beats ${computer}` : `You Lose! ${computer} beats ${player}`;
+            break;
+    }
+
+    return message;
+}
+
+console.log(playRound("ROCK", computerChoice()));
+console.log(playRound("PAPER", computerChoice()));
+console.log(playRound("SCISSORS", computerChoice()));
