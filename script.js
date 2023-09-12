1d700cde1e0b60c6a8c6a5553175cb31e7a2c9bc
Walter Hernandez <63087242+whernandezt@users.noreply.github.com>
2023-09-11 20:05:52 -0600

    Change functions logic
    Delete game funtion because it's no longer needed and set the player
    choice base on the clicked button. Show the player the result when
    winning or lossing 5 rounds.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+let result = [0,0];
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(btn => btn.addEventListener('click', () => {
+    playRound(btn.textContent.toUpperCase(),computerChoice());
+    console.log(result);
+    if(result[0] === 5 || result[1] === 5)
+    {
+        const divResul = document.querySelector('#result');
+        divResul.textContent = result[0] === 5 ? 'Congratulations! You win.':'Sorry! You lose.';
+    }
+}));
+
+const playRound = (player, computer) => {
+    if (computer === player)
+        return;
+
+    switch(player){
+        case "ROCK":
+            computer === "SCISSORS" ? result[0]++ : result[1]++;
+            break;
+        case "PAPER":
+            computer === "ROCK" ?  result[0]++ : result[1]++;
+            break;
+        case "SCISSORS":
+            computer === "PAPER" ? result[0]++ : result[1]++;
+            break;
+    }
+
+    return;
+}
+
 const computerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
 
@@ -13,64 +45,3 @@ const computerChoice = () => {
 
     return "SCISSORS";
 }
-
-const playRound = (player, computer) => {
-    if (computer === player)
-        return `It's a draw, both chose ${player}`;
-
-    let message = "";
-
-    switch(player){
-        case "ROCK":
-            message = computer === "SCISSORS" ? `You Win! ${player} beats ${computer}` : `You Lose! ${computer} beats ${player}`;
-            break;
-        case "PAPER":
-            message = computer === "ROCK" ? `You Win! ${player} beats ${computer}` : `You Lose! ${computer} beats ${player}`;
-            break;
-        case "SCISSORS":
-            message = computer === "PAPER" ? `You Win! ${player} beats ${computer}` : `You Lose! ${computer} beats ${player}`;
-            break;
-    }
-
-    return message;
-}
-
-const game = () => {
-    let playerChoice = "";
-    let rounds = 5;
-    let playerScore = 0;
-    let computerScore = 0;
-    let result = "";
-    
-    for(i = 0; i < rounds; i++){
-        while (playerChoice !== "ROCK" && playerChoice !== "PAPER" && playerChoice !== "SCISSORS")
-        {
-            playerChoice = prompt("Type Rock, Paper or Scissors:").toUpperCase();
-        }
-
-        result = playRound(playerChoice, computerChoice());
-
-        if(result.includes("Win")){
-            playerScore++;
-        }
-
-        if(result.includes("Lose")){
-            computerScore++;
-        }
-
-        console.log(`ROUND #${(i + 1)}: ${result}`);
-        playerChoice = "";
-    }
-
-    if(playerScore === computerScore){
-        console.log("It's a Draw!");
-    }
-    else if(playerScore > computerScore){
-        console.log(`You Win! You Score ${playerScore} vs ${computerScore} of ${rounds}`);
-    }
-    else {
-        console.log(`You Lose! You Score ${playerScore} vs ${computerScore} of ${rounds}`);
-    }
-}
-
-//game();
