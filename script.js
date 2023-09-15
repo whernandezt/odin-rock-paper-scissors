211100d83accf5473e4a9697ac9fbc30accedd45
Walter Hernandez <63087242+whernandezt@users.noreply.github.com>
2023-09-15 16:52:04 -0600

    Add running score

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let result = [0,0];
+let rounds = 0;
 
-const buttons = document.querySelectorAll('.btn');
+const buttons = document.querySelectorAll('.btnOption');
+
 buttons.forEach(btn => btn.addEventListener('click', () => {
     playRound(btn.textContent.toUpperCase(),computerChoice());
+    const score = document.querySelector('.score');
+    score.textContent = `Score: ${result[0]}/${rounds}`
     console.log(result);
     if(result[0] === 5 || result[1] === 5)
     {
+        const winner = document.createElement('h3');
+        winner.classList.add('winner');
+        winner.textContent = result[0] === 5 ? 'Congratulations! You win.':'Sorry! You lose.';
         const divResul = document.querySelector('#result');
-        divResul.textContent = result[0] === 5 ? 'Congratulations! You win.':'Sorry! You lose.';
+        divResul.appendChild(winner);
     }
 }));
 
 const playRound = (player, computer) => {
+    rounds++;
+
     if (computer === player)
         return;
 
